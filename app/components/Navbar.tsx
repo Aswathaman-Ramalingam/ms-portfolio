5798eae4a721448b19d689127ffc8ba11e77dfa1
Aswathaman-Ramalingam <aswathaman.official@gmail.com>
2025-08-04 22:13:49 +0530

    actual init

---
+++ app/components/Navbar.tsx
@@ -0,0 +1,14 @@
+export default function Navbar() {
+    return (
+        <nav className="flex items-center justify-end p-4 m-1">
+            <ul className="flex space-x-4">
+                <li>
+                    Home
+                </li>
+                <li>
+                    About
+                </li>
+            </ul>
+        </nav>
+    );
+}
